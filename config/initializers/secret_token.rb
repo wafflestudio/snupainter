a555adb006b636ddf09aa5974380779ac3aff98d
Beomki Lee <c0untd0wn@wafflestudio.com>
2012-11-19 13:26:18 +0900

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snupainter::Application.config.secret_token = '583972875c9d9d3add0d123b4190178910ea86f72ef50179ee1b19e4da640927739822d3b76896316289019abb09d47248b5d1c9bcece285c2f82ee7e5616823'
